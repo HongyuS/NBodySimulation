ded240058068208f7abefb972d9e819b1095e6cb
Hongyu Shi <44023928+HongyuS@users.noreply.github.com>
2022-06-09 19:27:09 +0800

    Initial Commit

---
+++ NBodySimulation/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  NBodySimulation
+//
+//  Created by Hongyu Shi on 2022/6/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NBodySimulation/NBodySimulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBodySimulationApp.swift
+//  NBodySimulation
+//
+//  Created by Hongyu Shi on 2022/6/9.
+//
+
+import SwiftUI
+
+@main
+struct NBodySimulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
